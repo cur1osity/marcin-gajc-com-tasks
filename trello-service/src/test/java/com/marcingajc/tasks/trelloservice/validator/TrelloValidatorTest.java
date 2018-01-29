cdaaf93098833788a7ea802dd4724c555a58e3ca
cur1osity <marcin.gajc@gmail.com>
2018-01-29 16:41:25 +0100

    Added trelloValidator tests

--- trello-service/src/test/java/com/marcingajc/tasks/trelloservice/validator/TrelloValidatorTest.java
+++ trello-service/src/test/java/com/marcingajc/tasks/trelloservice/validator/TrelloValidatorTest.java
@@ -1,7 +1,37 @@
 package com.marcingajc.tasks.trelloservice.validator;
 
-import static org.junit.Assert.*;
+import com.marcingajc.tasks.trelloservice.domain.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+import java.util.ArrayList;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(MockitoJUnitRunner.class)
 public class TrelloValidatorTest {
 
+    @InjectMocks
+    private TrelloValidator trelloValidator;
+
+    @Test
+    public void whenValidateTrelloBoardsShouldFilteredTrelloBoardWithNameTest() {
+
+        //Given
+        TrelloList trelloList = new TrelloList("1", "TEST", true);
+        List<TrelloList> trelloLists = new ArrayList<>();
+        trelloLists.add(trelloList);
+        TrelloBoard trelloBoard1 = new TrelloBoard("1","tEsT",trelloLists);
+        TrelloBoard trelloBoard2 = new TrelloBoard("2","AnotherName",trelloLists);
+        List<TrelloBoard> trelloBoards = new ArrayList<>();
+        trelloBoards.add(trelloBoard1);
+        trelloBoards.add(trelloBoard2);
+
+        //When
+        List<TrelloBoard> filteredBoards = trelloValidator.validateTrelloBoards(trelloBoards);
+
+        //Then
+        assertThat(filteredBoards.size()).isEqualTo(1);
+    }
 }
